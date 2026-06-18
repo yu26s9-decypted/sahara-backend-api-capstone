0a2143b07e205098a3331b11c82215ff6e930504
andy <andyt.cyb@gmail.com>
2026-06-18 19:51:31 -0400

    implemented POST endpoint:

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,11 +1,12 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.CategoryService;
@@ -34,24 +35,41 @@ public class ShoppingCartController
 
 
 
+    // I realize if I continue to implement postCart following the same pattern was getCart, this will violate DRY principle so i made a
+    // helper method for it!
+
+    private int getUserId(Principal principal){
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+
+        return user.getId();
+    }
     // each method in this controller requires a Principal object as a parameter
     @PreAuthorize("isAuthenticated()")
     @GetMapping()
     public ShoppingCart getCart(Principal principal)
     {
-        // get the currently logged in username
-        String userName = principal.getName();
-        // find database user by username
-        User user = userService.getByUserName(userName);
-        int userId = user.getId();
 
-        // use the shoppingCartService to get all items in the cart and return the cart
-        return shoppingCartService.getByUserId(userId);
+        return shoppingCartService.getByUserId(getUserId(principal));
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be added)
     // return the updated cart with status 201 Created
+
+//    @PostMapping("")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    public ResponseEntity<Category> addCategory(@RequestBody Category category)
+//    {
+//        System.out.println("hit");
+//        // insert the category and return it with status 201 Created
+//        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(category));
+//    }
+    @PostMapping("/product/{productId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ShoppingCart> postCart(@PathVariable int productId, Principal principal){
+       return null; // to complete when we implement service layer
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
